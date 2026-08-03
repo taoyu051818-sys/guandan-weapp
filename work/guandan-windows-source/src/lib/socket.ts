d10b190a9ccdb4046df37514ec91cb0d64428842
taoyu051818-sys <taoyu051818@gmail.com>
2026-08-03 12:05:14 +0800

    feat: import guandan migration source

---
+++ work/guandan-windows-source/src/lib/socket.ts
@@ -0,0 +1,17 @@
+import { io, Socket } from 'socket.io-client';
+
+export const socket: Socket = io({
+  autoConnect: false,
+});
+
+export const connectSocket = () => {
+  if (!socket.connected) {
+    socket.connect();
+  }
+};
+
+export const disconnectSocket = () => {
+  if (socket.connected) {
+    socket.disconnect();
+  }
+};
