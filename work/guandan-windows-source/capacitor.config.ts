16303f895e61bdce5389d921c285dd16ba4db7ac
taoyu051818-sys <taoyu051818@gmail.com>
2026-08-03 12:02:37 +0800

    feat: import guandan migration source

---
+++ work/guandan-windows-source/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guandan.master',
+  appName: '掼蛋大师',
+  webDir: 'dist'
+};
+
+export default config;
