a78c180d602ba45f56d84dad9a2d411289004d47
taoyu051818-sys <taoyu051818@gmail.com>
2026-08-03 12:02:05 +0800

    feat: import guandan migration source

---
+++ work/guandan-weapp/src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  pages: ['pages/index/index'],
+  window: {
+    navigationBarTitleText: '掼蛋大师',
+    navigationBarBackgroundColor: '#0b0c10',
+    navigationBarTextStyle: 'white',
+    backgroundColor: '#0b0c10'
+  }
+})
